14595d94e17a123d2e05a261ddf4cf2f820d90bf
Truong <Truong@Truong_PC.home>
2015-01-20 18:21:25 -0500

    Displaying chess board finished

---
+++ Chess/src/Game/ChessGame.java
@@ -0,0 +1,12 @@
+package Game;
+
+import Board.ChessBoard;
+
+public class ChessGame {
+
+	public static void main(String[] args) {
+		
+		ChessBoard chessBoard = new ChessBoard();
+		chessBoard.displayChessBoard();
+	}
+}
